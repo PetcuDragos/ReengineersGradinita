47c224540b6312d22eb3ebffc00a425d25506919
Dragos <dyenigma@gmail.com>
2022-05-19 00:30:43 +0300

    Added more assets

---
+++ Assets/FirstGameAssets/FirstGameScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstGameScript : MonoBehaviour
+{
+
+    public static float score = 0;
+    private int currentItemIndex = -1;
+    public GameObject[] items;
+    public GameObject currentGame;
+    public GameObject nextGame;
+    public AudioSource audioSource;
+    public AudioClip intro;
+    public AudioClip final;
+    private bool endGameHandlerStarted = false;
+    // Start is called before the first frame update
+
+    private void ShowNextItem()
+    {
+        currentItemIndex++;
+        items[currentItemIndex].SetActive(true);
+    }
+
+    void Start()
+    {
+        audioSource.clip = intro;
+        audioSource.Play();
+        ShowNextItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentItemIndex >= 0 && !items[currentItemIndex].activeInHierarchy)
+        {
+            if(currentItemIndex < items.Length - 1)
+            {
+                ShowNextItem();
+            }
+            else if(!endGameHandlerStarted)
+            {
+                StartEndGame();
+            }
+        }
+        if(endGameHandlerStarted && !audioSource.isPlaying)
+        {
+            EndGame();
+        }
+    }
+
+    private void StartEndGame()
+    {
+        endGameHandlerStarted = true;
+        audioSource.clip = final;
+        audioSource.Play();
+    }
+
+    private void EndGame()
+    {
+        currentGame.SetActive(false);
+        nextGame.SetActive(true);
+    }
+}
---
+++ Assets/FirstGameAssets/ItemScript.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class ItemScript : MonoBehaviour
+{
+    public GameObject[] correctTargets;
+    public GameObject[] wrongTargets;
+    private bool isBeingHeld = false;
+    public AudioSource audioSource;
+    public AudioClip wrong;
+    public AudioClip correct;
+
+    public void OnMouseDown()
+    {
+        isBeingHeld = true;
+
+    }
+    public void OnMouseUp()
+    {
+        isBeingHeld = false;
+
+    }
+
+    private void Start()
+    {
+        for(int i =0; i < correctTargets.Length; i ++)
+        {
+            correctTargets[i].SetActive(true);
+        }
+        for (int i = 0; i < wrongTargets.Length; i++)
+        {
+            wrongTargets[i].SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePos);
+
+            for (int i = 0; i < correctTargets.Length; i++)
+            {
+                if (correctTargets[i].GetComponent<Collider2D>().IsTouching(this.gameObject.GetComponent<Collider2D>()))
+                {
+                    FirstGameScript.score += 1;
+                    this.gameObject.SetActive(false);
+                    audioSource.clip = correct;
+                    audioSource.Play();
+                }
+            }
+            for (int i = 0; i < wrongTargets.Length; i++)
+            {
+                if (wrongTargets[i].GetComponent<Collider2D>().IsTouching(this.gameObject.GetComponent<Collider2D>()))
+                {
+                    FirstGameScript.score -= 0.2f;
+                    wrongTargets[0].SetActive(false);
+                    audioSource.clip = wrong;
+                    audioSource.Play();
+                }
+            }
+
+        }
+    }
+}
